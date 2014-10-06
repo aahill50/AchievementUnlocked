4f45d4df74b04f229a35acc836ce1c96593e3488
aahill50, halfwolf <student@appacademy.io>
2014-10-06 10:34:12 -0400

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AchievementUnlocked::Application.config.secret_key_base = '5169ade68537a891fe4a68c89e5cad0066d56062290506a17c90b7f5c7fb385f5d2f68a4480ad1f8e7dc875d01d568e70d5cc806f1170a7c6d264fed6cf02f64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AchievementUnlocked::Application.config.session_store :cookie_store, key: '_AchievementUnlocked_session'
